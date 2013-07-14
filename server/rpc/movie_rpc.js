f044ce216185884803e41e5177513bb7e5236555
Yutong Pei <yutongp@gmail.com>
2013-07-13 19:32:51 -0700

    add data structure for app

--- server/rpc/movie_rpc.js
+++ server/rpc/movie_rpc.js
@@ -1,9 +1,78 @@
+function MovieEvent (eventid, eventHost, lo, ti) {
+	this.participates = {};
+	this.comrecoMoives = {};
+	this.loca = lo;
+	this.time = ti;
+	this.eventID = eventid;
+	this.selectedMovies = {};
+	this.host = eventHost;
+
+	this.addParticipate (parti) {
+		if (this.participates[parti.fbID] == undefined) {
+			this.participates[parti.fbID] = new Participate();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	this.addComrecoMovies(movie) {
+		if (this.comrecoMoives[movie.movieID] == undefined) {
+			this.comrecoMoives[movie.movieID] = movie;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	this.addSelectedMovies(movie) {
+		if (this.selectedMovies[movie.movieID] == undefined) {
+			this.selectedMovies[movie.movieID] = movie;
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+function Participate () {
+	this.name = "";
+	this.fbID = "";
+	this.photourl = "";
+	this.recommandMoives = {};
+	this.friendList = {};
+	this.isHost = false;
+	this.online = false;
+
+	this.addRecommandMovies(movie) {
+		if (this.recommandMoives[movie.movieID] == undefined) {
+			this.recommandMoives[movie.movieID] = movie;
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+function Movie () {
+	this.movieID = "";
+	this.title = "";
+	this.imgurl = "";
+	this.score = 0;
+	this.pgRate = "";
+	this.description = "";
+	this.vote = 0;
+}
+
+allEvent = {};
+
 exports.actions = function(req, res, ss) {
 
 	// Example of pre-loading sessions into req.session using internal middleware
 	req.use('session');
 	var query = require('url').parse(req.url,true).query;
 	console.log(query);
+
 
 	return {
 		joinEvent: function (eventID) {
